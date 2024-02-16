76990d858727cf4c6ab13ab90a26f5c2b2b0ac41
Jake Diamond <83182690+Jake-Diamond-9@users.noreply.github.com>
2024-02-15 20:52:07 -0500

    calculating signal noise fraction

--- instantaneous_uncertainty_analysis.py
+++ instantaneous_uncertainty_analysis.py
@@ -49,8 +49,13 @@ def instantaneous_uncertainty_analysis(sdf_out, vc_out, cen, **inputs):
     # unpack needed variables
     fs = sdf_out['fs']
     time = sdf_out['time']
-    voltage = sdf_out['voltage']
+    #voltage = sdf_out['voltage']
     voltage_filt = vc_out['voltage_filt']
+    t_doi_start = sdf_out['t_doi_start']
+    t_doi_end = sdf_out['t_doi_end']
+    #t_start_corrected = sdf_out['t_start_corrected']
+    #t_before = inputs['t_before']
+    #t_after = inputs['t_after']
 
     # take only real component of the filtered voltage signal
     voltage_filt = np.real(voltage_filt)
@@ -61,63 +66,62 @@ def instantaneous_uncertainty_analysis(sdf_out, vc_out, cen, **inputs):
 
     # get the data for only the beginning section of the signal
     time_cut = time[0:steps_take]
-    voltage_cut = voltage[0:steps_take]
+    #voltage_cut = voltage[0:steps_take]
     voltage_filt_cut = voltage_filt[0:steps_take]
 
 
     # fit a sinusoid to the data
-    popt, pcov = curve_fit(sin_func, time_cut, voltage_cut, p0=[0.1, cen, 0, 0])
-    popt_filt, pcov_filt = curve_fit(sin_func, time_cut, voltage_filt_cut, p0=[0.1, cen, 0, 0])
-
+    popt, pcov = curve_fit(sin_func, time_cut, voltage_filt_cut, p0=[0.1, cen, 0, 0])
 
     # calculate the fitted curve
     volt_fit = sin_func(time_cut, popt[0], popt[1], popt[2], popt[3])
-    volt_fit_filt = sin_func(time_cut, popt_filt[0], popt_filt[1], popt_filt[2], popt_filt[3])
 
     # calculate the residuals
-    noise = voltage_cut - volt_fit
-    noise_filt = voltage_filt_cut - volt_fit_filt
+    noise = voltage_filt_cut - volt_fit
 
-    '''
+
     # calculate the envelope indices of the originally imported voltage data (and now filtered) using the stack
     # overflow code
-    lmin, lmax = hl_envelopes_idx(voltage_cut, dmin=10, dmax=10, split=False)
+    lmin, lmax = hl_envelopes_idx(voltage_filt, dmin=1, dmax=1, split=False)
 
     # interpolate the voltage envelope to every time point
-    env_max_interp = np.interp(time, time[lmax], voltage_filt_abs[lmax])
-    env_min_interp = np.interp(time, time[lmin], voltage_filt_abs[lmin])
-    '''
+    env_max_interp = np.interp(time, time[lmax], voltage_filt[lmax])
+    env_min_interp = np.interp(time, time[lmin], voltage_filt[lmin])
 
+    # calculate the estimated amplitude at every time
+    inst_amp = env_max_interp - env_min_interp
+
+    # calculate the estimated noise fraction at every time
+    inst_noise = np.mean(np.abs(noise)) / inst_amp
+
+    # find max noise on the domain of interest
+    t_doi_start_idx = np.argmin(np.abs(time - t_doi_start))
+    t_doi_end_idx = np.argmin(np.abs(time - t_doi_end))
+    doi_max_noise = np.max(inst_noise[t_doi_start_idx:t_doi_end_idx])
 
     # plotting
-    y_lim_volt = [-300, 300]
-    x_lim_noise = [-50, 50]
-
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, dpi=300)
-    ax1.plot(time_cut / 1e-9, voltage_cut*1e3, c='tab:blue')
+    ax1.plot(time_cut / 1e-9, voltage_filt_cut*1e3, c='tab:blue')
     ax1.plot(time_cut / 1e-9, volt_fit*1e3, c='tab:orange')
     ax1.set_xlabel('Time (ns)')
     ax1.set_ylabel('Voltage (mV)')
-    ax1.set_ylim(y_lim_volt)
 
+    ax2.hist(noise * 1e3, bins=30, rwidth=0.8)
+    ax2.set_xlabel('Noise (V)')
+    ax2.set_ylabel('Counts')
 
-    ax2.plot(time_cut / 1e-9, voltage_filt_cut * 1e3, c='tab:blue')
-    ax2.plot(time_cut / 1e-9, volt_fit_filt*1e3, c='tab:orange')
-    ax2.set_xlabel('Time (ns)')
-    ax2.set_ylabel('Voltage (mV)')
-    ax2.set_ylim(y_lim_volt)
+    ax3.plot(time / 1e-9, voltage_filt * 1e3, c='tab:blue')
+    ax3.plot(time / 1e-9, env_max_interp*1e3, c='tab:red')
+    ax3.plot(time / 1e-9, env_min_interp*1e3, c='tab:red')
+    ax3.set_xlabel('Time (ns)')
+    ax3.set_ylabel('Voltage (mV)')
+    ax3.set_xlim([t_doi_start / 1e-9, t_doi_end / 1e-9])
 
-
-    ax3.hist(noise*1e3, bins=30, rwidth=0.8)
-    ax3.set_xlabel('Noise (V)')
-    ax3.set_ylabel('Counts')
-    ax3.set_xlim(x_lim_noise)
-
-    ax4.hist(noise_filt * 1e3, bins=30, rwidth=0.8)
-    ax4.set_xlabel('Noise (V)')
-    ax4.set_ylabel('Counts')
-    ax4.set_xlim(x_lim_noise)
-
+    ax4.plot(time / 1e-9, inst_noise*100)
+    ax4.set_xlabel('Time (ns)')
+    ax4.set_ylabel('Noise Fraction (%)')
+    ax4.set_xlim([t_doi_start / 1e-9, t_doi_end / 1e-9])
+    ax4.set_ylim([0, doi_max_noise*110])
 
     plt.tight_layout()
     plt.show()
